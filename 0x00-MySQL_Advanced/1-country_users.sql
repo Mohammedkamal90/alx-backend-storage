b5466d216f4df0d170708a6c9697c672a35d40e4
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-20 23:57:31 +0000

    task1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Task 1: in and not out - create table users
+-- execute on any database
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
