216c78c0f205a5ec5820312c3165cde9c44a18ca
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-21 00:20:01 +0000

    task3

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,5 @@
 -- Task 2: best band ever! - rank country
--- ordered by the number of (non-fans)
-
+-- ordered by number of non-fans
 SELECT origin, SUM(nb_fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
