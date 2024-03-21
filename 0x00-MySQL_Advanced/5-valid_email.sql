71b4da727c0f22d279c89af8018dc5956196b9e7
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-21 00:27:27 +0000

    task6

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- Task 5: email validation to sent - Create trigger
+DELIMITER //
+
+CREATE TRIGGER reset_valid_email_on_email_change
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+//
+
+DELIMITER ;
